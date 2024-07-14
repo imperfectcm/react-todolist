0a8d945c9cd93bfd21eb0971ef34c230f4b23858
unknown <imperfect.cm@gmail.com>
2024-07-14 22:15:09 +0800

    ready to deploy

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
 import { TodoItem } from "./TodoItem";
 
+interface TodoListProps {
+    items: String[];
+}
 
-export function TodoList() {
-    const [itemList, setItemList] = useState<String[]>([
-        "Buy Banana",
-        "Buy Milk",
-        "Buy Cherry"
-    ]);
+export function TodoList(props: TodoListProps) {
+    //     state   , state updater
+    const [itemList, setItemList] = useState<String[]>(props.items);
+
+    const [input, setInput] = useState<String>("");
+
+    const addItem = () => {
+        setItemList([...itemList, input]);
+        setInput("");
+    }
 
     return (
         <div>
             <h2>Todo List</h2>
             <p>By CM</p>
-            <input type="text" placeholder="Todo content" />
-            <button>Add</button>
-            {itemList.map(item => (
-                <TodoItem item={item} />
+            <input type="text" placeholder="Todo content"
+                onChange={(e) => { 
+                    setInput(e.target.value);
+                }}
+                value={input+""}
+            />
+            <button onClick={addItem}>Add</button>
+            {itemList.map((item, index) => (
+                <TodoItem key={index} item={item} />
             ))}
         </div>
     )
